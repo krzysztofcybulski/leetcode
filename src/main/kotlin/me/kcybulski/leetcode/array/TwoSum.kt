a53d3d23cb8e94ae76297917219ade9f6240b15e
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2022-06-04 22:32:08 +0200

    TwoSum first implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/TwoSum.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/TwoSum.kt
@@ -3,7 +3,19 @@ package me.kcybulski.leetcode.array
 class TwoSum {
 
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        TODO()
+        val positionsToSum = nums
+            .mapIndexed { index, num -> num to index }
+            .groupBy { it.first }
+            .mapValues { it.value.map { v -> v.second } }
+
+        val firstThatCanSum = nums
+            .filter { num -> target - num in positionsToSum }
+            .first { num -> target - num != num || positionsToSum[num]!!.size > 1 }
+
+        return intArrayOf(
+            positionsToSum[firstThatCanSum]!!.first(),
+            positionsToSum[target - firstThatCanSum]!!.last()
+        )
     }
 
 }
