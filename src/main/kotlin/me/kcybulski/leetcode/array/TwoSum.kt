79936c8cc77bc8a2662edbb60509f6d1ec74f86e
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2022-06-04 22:44:02 +0200

    TwoSum refactor

--- src/main/kotlin/me/kcybulski/leetcode/array/TwoSum.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/TwoSum.kt
@@ -3,19 +3,16 @@ package me.kcybulski.leetcode.array
 class TwoSum {
 
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val positionsToSum = nums
-            .mapIndexed { index, num -> num to index }
-            .groupBy { it.first }
-            .mapValues { it.value.map { v -> v.second } }
+        val complements: MutableMap<Int, Int> = mutableMapOf()
 
-        val firstThatCanSum = nums
-            .filter { num -> target - num in positionsToSum }
-            .first { num -> target - num != num || positionsToSum[num]!!.size > 1 }
+        nums.forEachIndexed { index, num ->
+            if(target - num in complements) {
+                return intArrayOf(complements[target - num]!!, index)
+            }
+            complements[num] = index
+        }
 
-        return intArrayOf(
-            positionsToSum[firstThatCanSum]!!.first(),
-            positionsToSum[target - firstThatCanSum]!!.last()
-        )
+        error("No solution found")
     }
 
 }
