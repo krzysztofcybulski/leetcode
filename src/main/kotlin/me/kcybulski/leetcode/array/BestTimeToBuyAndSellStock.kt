94d33f9d32f22cf5541fa8d4841a877d5122dcb7
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2022-06-04 23:18:35 +0200

    BestTimeToBuyAndSellStock first implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/BestTimeToBuyAndSellStock.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/BestTimeToBuyAndSellStock.kt
@@ -1,9 +1,13 @@
 package me.kcybulski.leetcode.array
 
+import kotlin.math.max
+
 class BestTimeToBuyAndSellStock {
 
-    fun maxProfit(prices: IntArray): Int {
-        TODO()
-    }
-
+    fun maxProfit(prices: IntArray): Int =
+        prices
+            .foldRight(listOf(0)) { price, maxPrices -> listOf(max(price, maxPrices.first())) + maxPrices }
+            .dropLast(1)
+            .zip(prices.toList()) { a, b -> a - b }
+            .maxOrNull()!!
 }
