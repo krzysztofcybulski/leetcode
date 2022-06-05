98f2f40b69f6a95e80f4bc31124785c003fa9c74
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2022-06-05 13:20:48 +0200

    ContainsDuplicate first implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/ContainsDuplicate.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/ContainsDuplicate.kt
@@ -3,7 +3,12 @@ package me.kcybulski.leetcode.array
 class ContainsDuplicate {
 
     fun containsDuplicate(nums: IntArray): Boolean {
-
+        val savedNums: MutableSet<Int> = mutableSetOf()
+        nums.forEach { n ->
+            if(n in savedNums) return true
+            savedNums += n
+        }
+        return false
     }
 
 }
