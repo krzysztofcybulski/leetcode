618665a730a06197daa1776dc7897dd3e14c2ea6
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2022-06-05 13:49:55 +0200

    ProductOfArrayExceptSelf first implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/ProductOfArrayExceptSelf.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/ProductOfArrayExceptSelf.kt
@@ -3,7 +3,25 @@ package me.kcybulski.leetcode.array
 class ProductOfArrayExceptSelf {
 
     fun productExceptSelf(nums: IntArray): IntArray {
-        TODO()
+        return when (nums.count { it == 0 }) {
+            0 -> withoutZeros(nums)
+            1 -> oneZero(nums)
+            else -> IntArray(nums.size) { 0 }
+        }
+    }
+
+    private fun withoutZeros(nums: IntArray): IntArray {
+        val product = nums.reduce(Int::times)
+        return nums.map { product / it }.toIntArray()
+    }
+
+    private fun oneZero(nums: IntArray): IntArray {
+        val product = nums
+            .filterNot { it == 0 }
+            .reduce(Int::times)
+        val zeros = IntArray(nums.size) { 0 }
+        zeros[nums.indexOf(0)] = product
+        return zeros
     }
 
 }
