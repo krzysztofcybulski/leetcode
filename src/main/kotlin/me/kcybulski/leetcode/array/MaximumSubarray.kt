739a04db807d22fd11d99a9b434beb19d8255799
krzysztof.cybulski <krzysztof.cybulski@allegro.pl>
2022-09-09 14:23:35 +0200

    MaximumSubarray implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/MaximumSubarray.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/MaximumSubarray.kt
@@ -1,9 +1,21 @@
 package me.kcybulski.leetcode.array
 
+import java.lang.Integer.max
+
 class MaximumSubarray {
 
     fun maxSubArray(nums: IntArray): Int {
-        TODO()
+        var maxSoFar = nums[0]
+        var maxEndingHere = nums[0]
+
+        nums
+            .drop(1)
+            .forEach { num ->
+                maxEndingHere = max(maxEndingHere + num, num)
+                maxSoFar = max(maxSoFar, maxEndingHere)
+            }
+
+        return maxSoFar
     }
 
 }
