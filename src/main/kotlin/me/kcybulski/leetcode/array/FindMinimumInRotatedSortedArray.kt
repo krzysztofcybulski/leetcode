b2a1f684c0c1f25b35dc2ff65a964c1555060902
krzysztof.cybulski <krzysztof.cybulski@allegro.pl>
2022-09-11 10:39:27 +0200

    MaximumProductSubarray first implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/FindMinimumInRotatedSortedArray.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/FindMinimumInRotatedSortedArray.kt
@@ -3,7 +3,17 @@ package me.kcybulski.leetcode.array
 class FindMinimumInRotatedSortedArray {
 
     fun findMin(nums: IntArray): Int {
-        return 0
+        val mid = nums[nums.size / 2]
+        if(nums.size == 2) {
+            return if(nums[0] <= nums[1]) nums[0] else nums[1]
+        }
+        return if(mid < nums[0]) {
+            findMin(nums.sliceArray(0 ..nums.size / 2))
+        } else if(mid > nums.last()) {
+            findMin(nums.sliceArray(nums.size / 2 until nums.size))
+        } else {
+            nums.first()
+        }
     }
 
 }
