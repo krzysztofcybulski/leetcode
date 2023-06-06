affcd704e9e0459bbd0838e4ae78ebbbd21ad756
krzysztof.cybulski <krzysztof.cybulski@allegro.pl>
2023-06-06 21:31:39 +0200

    3Sum implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/ThreeSum.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/ThreeSum.kt
@@ -3,7 +3,23 @@ package me.kcybulski.leetcode.array
 class ThreeSum {
 
     fun threeSum(nums: IntArray): List<List<Int>> {
-        return emptyList()
+        val triplets = mutableSetOf<List<Int>>()
+        nums.sort()
+        nums
+            .dropLast(2)
+            .forEachIndexed { index, current ->
+                var left = index + 1
+                var right = nums.size - 1
+                while(left < right) {
+                    val sum = nums[left] + nums[right] + current
+                    when {
+                        sum == 0 -> triplets += listOf(nums[left++], nums[right--], current)
+                        sum > 0 -> right--
+                        sum < 0 -> left++
+                    }
+                }
+            }
+        return triplets.toList()
     }
 
 }
