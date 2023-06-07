dbd0755de2f90d38b0dd0112032d46580c6c511c
krzysztof.cybulski <krzysztof.cybulski@allegro.pl>
2023-06-07 14:27:05 +0200

    SumOfTwoIntegers test

---
+++ src/main/kotlin/me/kcybulski/leetcode/array/SumOfTwoIntegers.kt
@@ -0,0 +1,7 @@
+package me.kcybulski.leetcode.array
+
+class SumOfTwoIntegers {
+    fun getSum(a: Int, b: Int): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/me/kcybulski/leetcode/array/SumOfTwoIntegersTest.kt
@@ -0,0 +1,17 @@
+package me.kcybulski.leetcode.array
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class SumOfTwoIntegersTest : FunSpec({
+
+    val algorithm = SumOfTwoIntegers()
+
+    test("example 1") {
+        algorithm.getSum(1, 2) shouldBe  3
+    }
+
+    test("example 2") {
+        algorithm.getSum(2, 3) shouldBe  5
+    }
+})
