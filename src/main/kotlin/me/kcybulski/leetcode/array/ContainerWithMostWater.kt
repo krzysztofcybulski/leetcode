9292f43693add3befc1fdb5d004010ce4e1a0219
krzysztof.cybulski <krzysztof.cybulski@allegro.pl>
2023-06-07 14:25:08 +0200

    ContainerWithMostWater implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/ContainerWithMostWater.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/ContainerWithMostWater.kt
@@ -1,9 +1,25 @@
 package me.kcybulski.leetcode.array
 
+import kotlin.math.min
+
 class ContainerWithMostWater {
 
     fun maxArea(height: IntArray): Int {
-        return 0
+        var max = 0
+        var left = 0
+        var right = height.size - 1
+        while(left < right) {
+            val current = (right - left) * min(height[right], height[left])
+            if(current > max) {
+                max = current
+            }
+            if(height[right] < height[left]) {
+                right--
+            } else {
+                left++
+            }
+        }
+        return max
     }
 
 }
