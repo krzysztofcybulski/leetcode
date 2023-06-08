cb4e1de8faa4256ff7ff90f47d1c7b1296294edc
krzysztof.cybulski <krzysztof.cybulski@allegro.pl>
2023-06-08 17:52:32 +0200

    SumOfTwoIntegers implementation

--- src/main/kotlin/me/kcybulski/leetcode/array/SumOfTwoIntegers.kt
+++ src/main/kotlin/me/kcybulski/leetcode/array/SumOfTwoIntegers.kt
@@ -2,6 +2,9 @@ package me.kcybulski.leetcode.array
 
 class SumOfTwoIntegers {
     fun getSum(a: Int, b: Int): Int {
-        return 0
+        if(b == 0) {
+            return a
+        }
+        return getSum(a.xor(b), a.and(b).shl(1))
     }
 }
