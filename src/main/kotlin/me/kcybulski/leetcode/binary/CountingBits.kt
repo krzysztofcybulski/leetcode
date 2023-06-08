198743e1bec068645d76c8dd9a1b55fa9e20e40f
krzysztof.cybulski <krzysztof.cybulski@allegro.pl>
2023-06-08 22:54:00 +0200

    CountingBits implementation

--- src/main/kotlin/me/kcybulski/leetcode/binary/CountingBits.kt
+++ src/main/kotlin/me/kcybulski/leetcode/binary/CountingBits.kt
@@ -2,6 +2,11 @@ package me.kcybulski.leetcode.binary
 
 class CountingBits {
     fun countBits(n: Int): IntArray {
-        return IntArray(0)
+        val array = IntArray(n + 1)
+        array[0] = 0
+        for (i in 1..n) {
+            array[i] = array[i.shr(1)] + i.and(1)
+        }
+        return array
     }
 }
